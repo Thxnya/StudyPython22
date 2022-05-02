f8fcb53bc2eb388f173248bbaa7e36fb4aa9f166
ThxNya <anmin621@naver.com>
2022-05-02 17:50:45 +0900

    220502 while02 오류난 것 수정

--- basic/while02.py
+++ basic/while02.py
@@ -9,19 +9,19 @@ while True:
 4. 회원삭제
 5. 종료
 입력번호 :''')
-num = int(input()) # 키보드로 입력받은 수를 num할당
+    num = int(input()) # 키보드로 입력받은 수를 num할당
 
-if num == 1:
-    print('회원정보입력')
-elif num == 2: # 1은 아니고 2이면
-    print('회원검색')
-elif num == 3: # 1, 2도 아니고 3이면
-    print('회원수정')
-elif num == 4: # 4라면
-    print('회원삭제')
-elif num == 5:
-    print('프로그램종료')
-    break
-else:
-    print('잘못 입력했습니다.')
-    continue
+    if num == 1:
+        print('회원정보입력')
+    elif num == 2: # 1은 아니고 2이면
+        print('회원검색')
+    elif num == 3: # 1, 2도 아니고 3이면
+        print('회원수정')
+    elif num == 4: # 4라면
+        print('회원삭제')
+    elif num == 5:
+        print('프로그램종료')
+        break
+    else:
+        print('잘못 입력했습니다.')
+        continue
