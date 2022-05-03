b1c447566dca7e146e4e0aeac4a9e2cba4715ff6
ThxNya <anmin621@naver.com>
2022-05-03 11:47:59 +0900

    220503 점심전 커밋

--- basic/star_print.py
+++ basic/star_print.py
@@ -1,8 +1,20 @@
 # * 찍기
+'''
 for x in range(1, 6):
-    for y in range(x, 6):
+    for y in range(x, 5):
+        print(' ', end='')
+    for y in range(1, x):
         print('*', end='')
-    print('')
+    print()
+'''
 
-print('Hello', end=' ')
-print('World')
+#print('Hello', end=' ')
+#print('World')
+'''
+for i in range(0, 5):
+	for j in range(0, 4-i):
+		print(end=" ")
+	for j in  range(0, i+1):
+		print("*", end=" ")
+	print()
+'''
