12e98b73523aeee94ade0a2bcfd6d45414a3a104
ThxNya <anmin621@naver.com>
2022-05-03 17:48:07 +0900

    220503 star print 연습

--- basic/star_print.py
+++ basic/star_print.py
@@ -1,12 +1,12 @@
 # * 찍기
-'''
+
 for x in range(1, 6):
     for y in range(x, 5):
         print(' ', end='')
-    for y in range(1, x):
-        print('*', end='')
+    for y in range(1, x+1):
+        print('*', end=' ')
     print()
-'''
+
 
 #print('Hello', end=' ')
 #print('World')
